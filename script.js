486ce3a43e39bc4319009575a828a36e4d00dad5
Rohit Sharma <rohit10012@gmail.com>
2023-07-31 14:25:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 async function generateQuote() {
-  //const response = await fetch("https://api.quotable.io/random");
-  const response = await fetch("https://api.api-ninjas.com/v1/quotes?category=happiness");
+  const response = await fetch("https://api.quotable.io/random");
   const data = await response.json();
   const quote = `"${data.content}" -${data.author}`;
   document.getElementById("quote").innerHTML = quote;
