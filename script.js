0fb0c3958cb6a76a0c8e9bea16b8f6dd1e4d8b86
Rohit Sharma <rohit10012@gmail.com>
2023-07-31 12:05:18 +0530

    Update script.js with new API from Quotes Ninja
    https://api-ninjas.com/api/quotes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 async function generateQuote() {
-  const response = await fetch("https://api.quotable.io/random");
+  //const response = await fetch("https://api.quotable.io/random");
+  const response = await fetch("https://api.api-ninjas.com/v1/quotes?category=happiness");
   const data = await response.json();
   const quote = `"${data.content}" -${data.author}`;
   document.getElementById("quote").innerHTML = quote;
